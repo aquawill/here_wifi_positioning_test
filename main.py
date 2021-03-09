7144e3e5161ba3e9143aee38fc30ce95b7bd14f0
ww <aquawill@msn.com>
2021-03-09 15:30:20 +0800

    Minor fix.

--- main.py
+++ main.py
@@ -9,21 +9,20 @@ import time
 
 
 def rev_geocoder(lat, lon):
-    rev_geocoder_url = 'https://revgeocode.search.hereapi.com/v1/revgeocode?at={},{}&limit=1&lang=zh-TW&apikey={}'.format(
-        lat,
-        lon, api_key)
-    r = requests.get(rev_geocoder_url)
-    result_title = json.loads(r.text).get('items')[0].get('title')
-    print('Address: {}'.format(result_title))
-    return result_title
+    hls_rev_geocoder_url = 'https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox={},{}&mode=retrieveAddress&maxresults=1&gen=9&apiKey={}'.format(lat, lon, api_key)
+    hls_rev_geocode_r = requests.get(hls_rev_geocoder_url)
+    address = json.loads(hls_rev_geocode_r.text)['Response']['View'][0]['Result'][0]['Location']['Address']['Label']
+    print('Address: {}'.format(address))
+    return address
 
 
 def mia_cicular_picture(lon, lat, radius, label):
     mia_url = 'https://image.maps.ls.hereapi.com/mia/1.6/mapview?c={},{}&u={}&w=1440&h=900&ml=cht' \
               '&ppi=250&apiKey={}&t=3&tx={},{};{}&txs=30'.format(lat, lon, radius, api_key, lat - 0.00005, lon, label)
     image_data = requests.get(mia_url).content
-    image_file_name = 'here_network_positioning_result_{}.jpg'.format(
-        time.strftime('%d_%b_%Y_%H:%M:%S', time.localtime(time.time())))
+    timestamp = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time()))
+    image_file_name = 'here_network_positioning_result_{}.jpg'.format(timestamp)
+    positioning_log.write('{}\t{}\t{}\t{}\n'.format(timestamp, wifi_scan_result, network_positioning_r.text, label))
     with open(image_file_name, 'wb') as handler:
         handler.write(image_data)
 
@@ -31,7 +30,6 @@ def mia_cicular_picture(lon, lat, radius, label):
 api_key = ''  # YOUR HERE API KEY
 positioning_headers = {'Content-Type': 'application/json'}
 positioning_url = 'https://pos.ls.hereapi.com/positioning/v1/locate?apiKey={}'.format(api_key)
-rev_geocoder_url = 'https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox='
 if platform.system() == 'Windows':
     results = subprocess.check_output(["netsh", "wlan", "show", "network", "bssid"])
     results = results.decode("ascii")  # needed in python 3
@@ -41,10 +39,11 @@ if platform.system() == 'Windows':
         if re.match('.*BSSID.*', element):
             mac_list.add('{"mac": "' + (element.split(' : ')[1]) + '"}')
 elif platform.system() == 'Darwin':
-    wifi_list = os.popen(
-        '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s -x').read()
-    open('wifi_plist.txt', mode='w').write(wifi_list)
-    hotspots_plist = plistlib.loads(str.encode(wifi_list), fmt=plistlib.FMT_XML)
+    command = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s -x'  # the shell command
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+    output = process.communicate()
+    wifi_list = open('wifi_plist.txt', mode='r', encoding='utf-8').read()
+    hotspots_plist = plistlib.loads(str.encode(output[0].decode('utf-8', errors='ignore')), fmt=plistlib.FMT_XML)
     mac_list = set()
     for hotspot in hotspots_plist:
         ssid_str = hotspot['SSID_STR']
@@ -59,13 +58,14 @@ elif platform.system() == 'Darwin':
                 bssid_modified_arr.append(bssid_elem)
             bssid = ':'.join(bssid_modified_arr)
         mac_list.add('{\"mac\":\"' + bssid + '\",\"powrx\":' + str(rssi) + '}')  # BSSID + Powrx
-data = '{"wlan":[' + ','.join(i for i in mac_list) + ']}'
-print('Wifi hotspots:\n' + data)
-req = requests.post(url=positioning_url, data=data, headers=positioning_headers)
-json_result = json.loads(req.text)
+wifi_scan_result = '{"wlan":[' + ','.join(i for i in mac_list) + ']}'
+print('Wifi hotspots:\n' + wifi_scan_result)
+network_positioning_r = requests.post(url=positioning_url, data=wifi_scan_result, headers=positioning_headers)
+json_result = json.loads(network_positioning_r.text)
 print('result:\n' + str(json_result))
 lat = json_result['location']['lat']
 lon = json_result['location']['lng']
+positioning_log = open('positioning_result_log.txt', mode='a', encoding='utf-8')
 radius = json_result['location']['accuracy']
 address_label = rev_geocoder(lat, lon)
 mia_cicular_picture(lon, lat, radius, address_label)
