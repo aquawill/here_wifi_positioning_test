033d0c3ed8e06fc2c129ce65fed8be167b796f7f
ww <aquawill@msn.com>
2022-07-01 11:47:43 +0800

    Upgrade to HERE Network Positioning API v2 and GS7.

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import time
 import requests
 
 
-def rev_geocoder(lat, lon):
+def rev_geocoder_7(lat, lon):
     rev_geocoder_url = 'https://revgeocode.search.hereapi.com/v1/revgeocode?at={},{}&limit=10&lang=zh-TW&apikey={}'.format(
         lat, lon, api_key)
+    # print(rev_geocoder_url)
     r = requests.get(rev_geocoder_url)
     rev_geocoder_result_items = json.loads(r.text).get('items')
     for rev_geocoder_result_item in rev_geocoder_result_items:
@@ -25,20 +26,11 @@ def rev_geocoder(lat, lon):
             continue
 
 
-# def rev_geocoder(lat, lon):
-#     hls_rev_geocoder_url = 'https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox={},{}&mode=retrieveAddress&maxresults=1&gen=9&apiKey={}'.format(lat, lon, api_key)
-#     hls_rev_geocode_r = requests.get(hls_rev_geocoder_url)
-#     address = json.loads(hls_rev_geocode_r.text)['Response']['View'][0]['Result'][0]['Location']['Address']['Label']
-#     print('Address: {}'.format(address))
-#     return address
-
-def mia_cicular_picture(lon, lat, radius, label):
+def mia_cicular_picture(lat, lon, radius, label):
     mia_url = 'https://image.maps.ls.hereapi.com/mia/1.6/mapview?c={},{}&u={}&w=1440&h=900&ml=cht' \
-              '&ppi=250&apiKey={}&t=3&tx={},{};{}&txs=30'.format(lat, lon, radius, api_key, lat - 0.00005, lon, label)
+              '&ppi=72&apiKey={}&t=3&tx={},{};{}&txs=30'.format(lat, lon, radius, api_key, lat - 0.00005, lon, label)
     image_data = requests.get(mia_url).content
-    timestamp = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time()))
     image_file_name = 'here_network_positioning_result_{}.jpg'.format(timestamp)
-    positioning_log.write('{}\t{}\t{}\t{}\n'.format(timestamp, wifi_scan_result, network_positioning_r.text, label))
     with open(image_file_name, 'wb') as handler:
         handler.write(image_data)
 
@@ -63,8 +55,7 @@ if platform.system() == 'Windows':
     while i < len(parsed_result_list):
         if i % 2 == 1:
             signal_percentage = int(parsed_result_list[i].replace('%', ''))
-            mac_list.add(
-                '{"mac": "' + parsed_result_list[i - 1] + '", "rss": ' + str(int((signal_percentage / 2) - 100)) + '}')
+            mac_list.add('{"mac": "' + parsed_result_list[i - 1] + '"}')
         i += 1
 elif platform.system() == 'Darwin':
     command = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s -x'  # the shell command
@@ -84,18 +75,21 @@ elif platform.system() == 'Darwin':
                     bssid_elem = '0{}'.format(bssid_elem)
                 bssid_modified_arr.append(bssid_elem)
             bssid = ':'.join(bssid_modified_arr)
-        mac_list.add('{\"mac\":\"' + bssid + '\",\"powrx\":' + str(rssi) + '}')  # BSSID + Powrx
+        mac_list.add('{\"mac\":\"' + bssid + '\"}')  # BSSID + Powrx
 wifi_scan_result = '{"wlan":[' + ','.join(i for i in mac_list) + ']}'
 print('Wifi hotspots:\n' + wifi_scan_result)
 network_positioning_r = requests.post(url=positioning_url, data=wifi_scan_result, headers=positioning_headers)
 if network_positioning_r.status_code == 200:
     json_result = json.loads(network_positioning_r.text)
-    print('result:\n' + str(json_result))
     lat = json_result['location']['lat']
     lon = json_result['location']['lng']
+    accuracy = json_result['location']['accuracy']
+    address_lat, address_lon, address_label = rev_geocoder_7(lat, lon)
     positioning_log = open('positioning_result_log.txt', mode='a', encoding='utf-8')
-    radius = json_result['location']['accuracy']
-    address_lat, address_lon, address_label = rev_geocoder(lat, lon)
-    mia_cicular_picture(lon, lat, radius, address_label)
+    timestamp = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time()))
+    positioning_log.write(
+        '{}\t{}\t{}\t{}\n'.format(timestamp, wifi_scan_result, network_positioning_r.text, address_label))
+    mia_cicular_picture(lat, lon, accuracy, address_label)
+    print('result:\n' + str(json_result) + '\t' + address_label)
 else:
     print(network_positioning_r.text)
